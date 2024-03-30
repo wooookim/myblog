75d25a648e292e64eada91e22176a411be2f2ead
wooookim <jhr01175@naver.com>
2024-03-30 17:34:39 +0900

    [ADD] 글 작성 페이지 [UPDATE] 댓글 작성

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from blog.models import Post
+from django.shortcuts import render, redirect
+from blog.models import Post, Comment
 
 
 # Create your views here.
@@ -15,7 +15,28 @@ def post_list(request):
 def post_detail(request, post_id):
     post = Post.objects.get(id=post_id)
 
+    if request.method == "POST":
+        comment_content = request.POST["comment"]
+        Comment.objects.create(
+            post=post,
+            content=comment_content,
+        )
+
     context = {
         "post": post,
     }
     return render(request, "post_detail.html", context)
+
+
+def post_add(request):
+    if request.method == "POST":
+        title = request.POST["title"]
+        content = request.POST["content"]
+        thumbnail = request.FILES["thumbnail"]
+        post = Post.objects.create(
+            title=title,
+            content=content,
+            thumbnail=thumbnail,
+        )
+        return redirect(f"/posts/{post.id}")
+    return render(request, "post_add.html")
--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from config.views import index
-from blog.views import post_list, post_detail
+from blog.views import post_list, post_detail, post_add
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -27,6 +27,7 @@ urlpatterns = [
     path("", index),
     path("posts/", post_list),
     path("posts/<int:post_id>/", post_detail),
+    path("posts/add/", post_add),
 ]
 
 urlpatterns += static(
